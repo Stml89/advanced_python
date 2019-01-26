41e9724f6c77cd656621b680ca39c57408bb1741
Siarhei Stamal <sergeistamal@gmail.com>
2019-01-27 00:08:25 +0300

    Add homework 5. Downloading bunch file from python.com../source

--- synchronization.py
+++ synchronization.py
@@ -1,42 +1,61 @@
-"""
-Output numbers from 0..100 in order.
-First thread outputs even numbers. Second thread outputs odd numbers.
-
-By synchronizing processes with
-Lock object and shared counter with Value object.
-"""
-
-import multiprocessing
+import aiohttp
+import asyncio
+import os
+import re
+import threading
 import time
+import urllib.request
 
+URL = 'https://www.python.org/downloads/source/'
 
-def print_numbers(thread_name, delay, counter):
-    with lock:
-        time.sleep(delay)
-        print("{} {}".format(thread_name, counter.value))
-        counter.value += 1
+def get_links_for_download():
+    response = urllib.request.urlopen(URL)
+    text = response.read()
+    return re.findall(r'Download <a href="(.*?)">', str(text))
 
+def download_bunch_of_files(link):
+    urllib.request.urlretrieve(link)
 
-def print_even_numbers(name, sleep_time, counter):
-    while counter.value <= 100:
-        if not counter.value % 2:
-            print_numbers(name, sleep_time, counter)
+def start_thr(links):
+    _list_of_thr = []
+    start_time = time.time()
 
+    for link in links:
+        t = threading.Thread(target=download_bunch_of_files, args=(link,))
+        t.start()
+        _list_of_thr.append(t)
+    for i in _list_of_thr:
+        i.join()
 
-def print_odd_numbers(name, sleep_time, counter):
-    while counter.value <= 100:
-        if counter.value % 2:
-            print_numbers(name, sleep_time, counter)
+    print("Download all bunches with threads from {},"
+          "took {:.2f} sec".format(URL, time.time()-start_time))
 
+@asyncio.coroutine
+async def download_coroutine(url):
+    async with aiohttp.ClientSession(loop=loop) as session:
+        async with session.get(url) as response:
+            filename = os.path.basename(url)
+            with open(filename, 'wb') as f_handle:
+                while True:
+                    chunk = await response.content.read(8192)
+                    if not chunk:
+                        break
+                    f_handle.write(chunk)
+            return await response.release()
 
-if __name__ == '__main__':
-    count = multiprocessing.Value('i', 0)
-    lock = multiprocessing.Lock()
+async def download_multiple(links):
+    start_time = time.time()
+    tasks = [asyncio.create_task(download_coroutine(link)) for link in links]
+    await asyncio.gather(*tasks)
+    print("Download all bunches with asyncio from {},"
+          " took {} sec".format(URL, time.time() - start_time))
 
-    process_1 = multiprocessing.Process(target=print_even_numbers,
-                                        args=("Process-1", 1, count))
-    process_2 = multiprocessing.Process(target=print_odd_numbers,
-                                        args=("Process-2", 1, count))
+links = get_links_for_download()
 
-    process_1.start()
-    process_2.start()
+print('Threads:')
+start_thr(links)
+
+print('Asynchronous:')
+loop = asyncio.get_event_loop()
+result = loop.run_until_complete(download_multiple(links))
+loop.close()
